4828253a780160f45fc674dfc1704a328726ff1a
Mrigank8989 <156689686+Mrigank8989@users.noreply.github.com>
2025-11-28 09:54:05 +0530

    Update quizAttemptModel.js

--- module/quizAttemptModel.js
+++ module/quizAttemptModel.js
@@ -4,8 +4,8 @@ const pool = require('../config/db');
 const insertQuizAttempt = async (attemptData) => {
   const query = `
     INSERT INTO quiz_attempts 
-      (user_id, quiz_id, score, total_questions, percentage, time_taken, attempt_date, is_completed)
-    VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+      (user_id, quiz_id, score, correct, wrong, unanswered, total_questions, percentage, time_taken, snapshot, attempt_date, is_completed)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
     RETURNING *;
   `;
 
@@ -13,9 +13,13 @@ const insertQuizAttempt = async (attemptData) => {
     attemptData.user_id,
     attemptData.quiz_id,
     attemptData.score,
+    attemptData.correct,
+    attemptData.wrong,
+    attemptData.unanswered,
     attemptData.total_questions,
     attemptData.percentage,
     attemptData.time_taken,
+    attemptData.snapshot || null,
     attemptData.attempt_date || new Date(),
     attemptData.is_completed
   ];
@@ -23,6 +27,7 @@ const insertQuizAttempt = async (attemptData) => {
   const result = await pool.query(query, values);
   return result.rows[0];
 };
+
 
 module.exports = {
   insertQuizAttempt
